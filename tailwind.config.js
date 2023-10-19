e1518800bd980bc40f4510e90d366a117e96d143
Manuela Gomez <manu.gomez.e@gmail.com>
2023-10-18 19:04:35 -0500

    adding tailwind, importing font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
  "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Inter': ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
