1fab9e00617995865e6ff9dda6a9020f079ff556
Manuela Gomez <manu.gomez.e@gmail.com>
2023-10-19 22:32:03 -0500

    recipeBox styling, added SVGs

--- src/components/RecipeBox.jsx
+++ src/components/RecipeBox.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import {HiOutlineHeart} from 'react-icons/hi';
+import {FaStar} from 'react-icons/fa6';
 
 function RecipeBox() {
     return (
-        <div className='flex flex-col justify-around w-1/5 aspect-square shadow-xl bg-gray-200 rounded-3xl m-2 pl-3'>
-            <button className='flex items-center h-1/6 w-1/5 bg-main-green rounded-tr-3xl rounded-bl-3xl self-end hover:bg-black'>
-                <img className='h-5 self-center' src='../../public/images/fav.svg'/>
+        //TODO: Hacer responsive a partir de 800px 
+        //! Buscar como organizar bien la caja (botón fijado arriba a la izquierda, ignorado por el resto)
+        <div className='flex flex-col static justify-around min-w-[300px] max-w-sm aspect-square shadow-xl rounded-3xl m-5 pl-6 pb-5'>
+            <button className='flex items-center relative top-0 right-0  h-1/6 w-1/5 min-w-[25%] bg-main-green rounded-tr-3xl rounded-bl-3xl self-end hover:bg-black'>
+                <HiOutlineHeart 
+                    className='m-auto w-1/2 h-1/2 stroke-2'
+                    stroke='white'
+                    size={20}
+                />
             </button>
-            <img className='h-1/2 w-1/2 self-center' src='../../public/images/pad-thai.png'/>
+            <img className='h-1/2 self-center' src='../../public/images/pad-thai.png'/>
                 <h2 className='font-semibold mt-1'>Pad Thai</h2>
-                <p className='font-semithin mt-1 text-gray-600'>Descripción de la receta</p>
+                <p className='text-gray-700 font-medium'>Descripción de la receta</p>
             <div className='flex justify-around pb-2'>
                 <p>90 min</p>
-                <p>calificación</p>
+                <div className='flex items-center'>
+                    <p className='pr-1'>4.5 </p>
+                    <FaStar 
+                        className='w-5 h-5'
+                        color='#FFE605'
+                        size={20}
+                    />
+                </div>
+                
             </div>
         </div>
     );
