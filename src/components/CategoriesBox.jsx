f5c208f2bb64efc9b613984db604d0fb1d9287a8
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-10-22 19:26:44 -0500

    changes in CategoriesBox

--- src/components/CategoriesBox.jsx
+++ src/components/CategoriesBox.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function CategoriesBox(){
     return(
-        <div className=''>
-            <button className=''>
-                <img className='' src='../../public/images/desayuno.png'/>
+        <div className='py-4 px-5 m-5 min-w-min max-w-md flex flex-col rounded-lg shadow-md shadow-gray-400 '>
+            <button className='bg-main-green bg-center rounded-full border-inherit p-4'>
+                <img className='h-20 justify-self-center' src='../../public/images/desayuno.png'/>
             </button>
-                <h2 className=''>Desayuno</h2>
-                <p className=''>(12 desayunos)</p>
+                <h2 className='text-center font-semibold font-Inter'>Desayuno</h2>
+                <p className='text-center font-thin font-Inter'>(12 desayunos)</p>
         </div>
         
     );
