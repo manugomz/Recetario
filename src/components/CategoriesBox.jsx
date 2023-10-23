f69375c422e7cc6a600280a36628610029145c8f
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-10-23 11:14:24 -0500

    new img

--- src/components/CategoriesBox.jsx
+++ src/components/CategoriesBox.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function CategoriesBox(){
     return(
-        <div className='py-4 px-5 m-5 min-w-min max-w-md flex flex-col rounded-lg shadow-md shadow-gray-400 '>
-            <button className='bg-main-green bg-center rounded-full border-inherit p-4'>
-                <img className='h-20 justify-self-center' src='../../public/images/desayuno.png'/>
+        <div className='py-4 px-5 m-5 min-w-326 max-w-md rounded-40 flex flex-col rounded-lg content-center shadow-xl shadow-gray-400 '>
+            <button className='bg-main-green bg-center hover:bg-dark-green rounded-full border-inherit p-4 '>
+                <img className='h-20 justify-self-center' src='../../public/images/Desayuno.png'/>
             </button>
                 <h2 className='text-center font-semibold font-Inter'>Desayuno</h2>
                 <p className='text-center font-thin font-Inter'>(12 desayunos)</p>
