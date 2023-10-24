36bdb09d03265d28ae071fda74f652a7484fd741
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-10-23 20:25:27 -0500

    changes CategoriesBox bg color

--- src/components/CategoriesBox.jsx
+++ src/components/CategoriesBox.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CategoriesBox(){
     return(
         <div className='py-4 px-5 w-60 m-5  aspect-square rounded-40 flex flex-col rounded-lg place-items-center shadow-xl shadow-gray-400 '>
-            <button className='bg-green-1git00 bg-center hover:bg-green-500 rounded-full border-inherit p-4 '>
+            <button className='bg-green-100 bg-center hover:bg-green-500 rounded-full border-inherit p-4 '>
                 <img className='h-16 justify-self-center' src='../../public/images/Desayuno.png'/>
             </button>
                 <h3 className='text-center font-semibold font-Inter'>Break Fast</h3>
