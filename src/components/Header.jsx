500b7aa28f334dfdc86c8351b8826d21714a757b
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-10-23 20:42:34 -0500

    changes in color in header and CategoriesBox

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,13 @@ function Header({titulo}) {
                 <a href='#!' className='font-semibold text-black text-xl font-Inter'>{titulo}</a>
             </div>
             <ul className='flex justify-around text-black text-lg font-semibold font-Inter'>
-                <a href="/"><li className='p-5 hover:text-main-green'>Home</li></a>
+                <a href="/"><li className='p-5 hover:text-green-300'>Home</li></a>
                 <a href="/categorias-populares"><li className='p-5 hover:text-main-green'>Popular Categories</li></a>
-                <a href="/recetas-populares"><li className='p-5 hover:text-main-green'>Popular Ingredients</li></a>
-                <a href="/about-us"><li className='p-5 hover:text-main-green'>About Us</li></a> 
+                <a href="/recetas-populares"><li className='p-5 hover:text-green-300'>Popular Ingredients</li></a>
+                <a href="/about-us"><li className='p-5 hover:text-green-300'>About Us</li></a> 
             </ul> 
             <img className='flex items-center w-6 h-6'  src='../../images/icon_search.png'/>
-            <button className='flex items-center bg-main-green pl-4 pr-4 h-10 rounded-full'>
+            <button className='flex items-center bg-green-300 pl-4 pr-4 h-10 rounded-full'>
                 <img className='w-5 h-5'  src='../../images/fi-rr-phone-call.png'/>
                 <div className='pl-2 pr-2 text-white font-semibold'>Contact</div>
             </button>    
