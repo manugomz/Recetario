7465d9ffe504010f0c6fe9abe4bef4036fe4c533
Manuela Gomez <manu.gomez.e@gmail.com>
2023-10-31 21:41:22 -0500

    added mini title and created a useFetch js file

--- src/containers/PopularRecipes.jsx
+++ src/containers/PopularRecipes.jsx
@@ -4,7 +4,10 @@ import RecipeBox from '../components/RecipeBox';
 function PopularRecipes() {
     return (
         <section>
-            <h1 className='font-extrabold text-4xl m-5 font-Inter'> Popular recipes</h1>
+            <h2 className="font-Inter font-bold 
+            text-red-500 text-center text-xs
+            text-sm ">SPECIAL DISHES</h2>
+            <h1 className='font-extrabold text-4xl font-Inter text-center mx-5 mb-5 '> Popular recipes</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-items-center p-auto w-screen'>
                 <RecipeBox/>
                 <RecipeBox/>
