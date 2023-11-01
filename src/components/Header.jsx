362d7a76fc15f9b7e83b9721165c38a0ec2f2bf6
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-10-31 22:25:38 -0500

    changing images path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header({titulo}) {
     return (
         <nav className='flex flex-row justify-around items-center'>
             <div className='flex items-center'>
-                <img className='w-10 h-10'  src='../../images/logoR.png'/>
+                <img className='w-10 h-10'  src='../src/assets/images/logoR.png'/>
                 <a href='#!' className='font-semibold text-black text-xl font-Inter'>{titulo}</a>
             </div>
             <ul className='flex justify-around text-black text-lg font-semibold font-Inter'>
@@ -15,9 +15,9 @@ function Header({titulo}) {
                 <a href="/popular-recipes"><li className='p-5 hover:text-green-300'>Popular Recipes</li></a>
                 <a href="/about-us"><li className='p-5 hover:text-green-300'>About Us</li></a> 
             </ul> 
-            <img className='flex items-center w-6 h-6'  src='../../images/icon_search.png'/>
+            <img className='flex items-center w-6 h-6'  src='../src/assets/images/icon_search.png'/>
             <button className='flex items-center bg-green-300 pl-4 pr-4 h-10 rounded-full'>
-                <img className='w-5 h-5'  src='../../images/fi-rr-phone-call.png'/>
+                <img className='w-5 h-5'  src='../src/assets/images/fi-rr-phone-call.png'/>
                 <div className='pl-2 pr-2 text-white font-semibold'>Contact</div>
             </button>    
         </nav>
