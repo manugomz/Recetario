db127bdf13ff8c078612caa26e923411103a0a25
Adriana Restrepo <adrianarestrepom@hotmail.com>
2023-11-01 19:55:29 -0500

    Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+
 import PopularCategories from './containers/PopularCategories'
 import PopularRecipes from './containers/PopularRecipes'
 import InfoAbout from './containers/InfoAbout'
