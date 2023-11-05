99b3463a73e32ebcf731ce3f6651bae89a7c5d51
Manuela Gomez <manu.gomez.e@gmail.com>
2023-11-04 22:43:33 -0500

    updated style of fav button

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -7,13 +7,12 @@ const FavoriteButton = () => {
   const handleFavoriteToggle = () => {setIsFavorite(!isFavorite)};
 
   return (
-    <button className={`flex items-center
-                        h-1/6 w-1/5 min-w-[25%]
-                        absolute top-0 right-0
+    <button className={`flex items-center absolute top-0 right-0
+                        h-[15%] w-[18%] p-1
                         bg-green-300 
-                        rounded-tr-[2.5rem] rounded-bl-[2.5rem] 
+                        rounded-tr-[2rem] rounded-bl-[2rem] 
                         hover:bg-green-500 hover:shadow-light hover:-translate-y-1 hover:duration-300
-                        [&>svg]:hover:scale-125 [&>svg]:duration-300
+                        [&>svg]:hover:scale-125 [&>svg]:duration-300 
                         active:bg-green-700 active:shadow-none active:translate-y-0 active:duration-0
                         ${isFavorite ? '[&>svg]:fill-white':'[&>svg]:fill-none' }
                     `}
@@ -21,7 +20,6 @@ const FavoriteButton = () => {
         <HiOutlineHeart 
             className='m-auto w-1/2 h-1/2 stroke-2'
             stroke='white'
-            size={20}
         />
     </button>
   );
--- src/containers/PopularRecipes.jsx
+++ src/containers/PopularRecipes.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import RecipeBox from '../components/RecipeBox';
+import Recipe from './Recipe';
 
 function PopularRecipes() {
     return (
         <section>
             <h2 className="font-Inter font-bold 
-            text-red-500 text-center text-xs
+                         text-red-500 text-center text-xs
             ">SPECIAL DISHES</h2>
             <h1 className='font-extrabold text-4xl font-Inter text-center mx-5 mb-5 '> Popular recipes</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 justify-items-center p-auto w-screen'>
+            <div className='grid grid-cols-1 
+                            w-screen
+                            justify-items-center 
+                            p-auto 
+                            md:grid-cols-2 
+                            lg:grid-cols-3 
+                            2xl:grid-cols-4'>
                 <RecipeBox/>
                 <RecipeBox/>
                 <RecipeBox/> 
-            </div>
+            </div> 
         </section>
     );
 }
