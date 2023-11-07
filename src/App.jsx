5d519ac414d59139853e0e756d0977d0c598ef11
Manuela Gomez <manu.gomez.e@gmail.com>
2023-11-06 21:20:37 -0500

    created 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-
 import PopularCategories from './containers/PopularCategories'
 import PopularRecipes from './containers/PopularRecipes'
 import InfoAbout from './containers/InfoAbout'
+import NotFound from './containers/NotFound'
 
 function App() {
 
@@ -20,6 +20,7 @@ function App() {
         <Route path="/popular-categories" element={<PopularCategories/>} />
         <Route path="/popular-recipes" element={<PopularRecipes/>} />
         <Route path="/about-us" element={<InfoAbout/>} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
       </BrowserRouter>
       <Footer/>
