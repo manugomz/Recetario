f61e28353ef872902dc3edaf2b32ffa36f0504b8
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-11-09 16:54:41 -0500

    changes in Contacts CSS and info

--- src/containers/Contacts.jsx
+++ src/containers/Contacts.jsx
@@ -3,16 +3,24 @@ import React from 'react';
 function Contacts () {
     return (
         <section>
-            <h1 className='font-Inter font-bold text-red-500 text-center leading-10 text-sm h-6 pb-10'>CONTAC US</h1>
-            <div className='flex flex-row justify-evenly '>
-                <div className='w-1/4 h-1/4 static'>
-                    <img className='h-80 absolute z-30' src="../../src/assets/images/Chef.png" alt="Chef" />
-                    <img className='h-80 absolute z-30 bottom-px' src="../../src/assets/images/Fondo.png" alt="Fondo" />
+            <div className='flex flex-col justify-around'>
+                <h1 className='font-Inter font-bold 
+                             text-red-500 text-center leading-10 text-sm 
+                            h-6 pb-10'>CONTAC US</h1>
+                <div className='flex flex-row justify-evenly relative place-items-center'>
+                    <div className='relative w-96 h-96'>
+                        <img className='absolute z-20 h-80 w-80' 
+                             src="../../src/assets/images/Chef.png" 
+                             alt="Chef" />
+                        <img className='absolute bottom-px' 
+                             src="../../src/assets/images/Fondo.png" 
+                             alt="Fondo" />
+                    </div>
+                    <div className='font-Inter text-center text-xl'>
+                        <p>Tel: +64 958 248 966</p>
+                        <p>Email: info@recetary.com</p>                
+                    </div> 
                 </div>
-                <div className='font-Inter w-1/4 h-1/4 static'>
-                    <p className=''>+64 958 248 966</p>
-                    <p className=''>info@recetary.com</p>                
-                </div> 
             </div>
         </section>
         
