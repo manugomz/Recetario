dc22f117c6b304f58ae6050abc3fdff420771a75
Manuela Gomez <manu.gomez.e@gmail.com>
2023-12-17 13:31:28 -0500

    removed unused images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
         >
           <img
             className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12"
-            src="./../src/assets/images/logoR.png"
+            src="../../src/assets/images/logoR.png"
           />
           ecetary.com
         </Link>
