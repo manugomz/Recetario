43e6ca015898ebc426cf8bc32b650c20c9672e1d
Manuela Gomez <manu.gomez.e@gmail.com>
2023-12-25 18:14:28 -0500

    change react router for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom';
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom';
 
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-     <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
