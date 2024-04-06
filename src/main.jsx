b4ac5ecf4caaa12ddc90255d43f95695e2741278
Manuela Gomez <manu.gomez.e@gmail.com>
2024-04-05 21:57:52 -0500

    Replace `HashRouter` with `BrowserRouter`

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
